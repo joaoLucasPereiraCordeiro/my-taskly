57e9a9dcdd532d3f8438d271b7bfb8e917408e4c
joaoLucasPereiraCordeiro <joaolucaspereiracordeiro@gmail.com>
2025-11-07 08:02:01 -0300

    Atualização do cors

--- todolist-backend/config/cors.php
+++ todolist-backend/config/cors.php
@@ -8,13 +8,13 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://my-taskly-frontend-avubnc9gb-joao-lucas-projects-1bb67238.vercel.app',
         'https://my-taskly-teal.vercel.app',
+        'https://my-taskly-frontend-avubnc9gb-joao-lucas-projects-1bb67238.vercel.app',
         'http://localhost:5173',
         'http://localhost:8080',
     ],
@@ -23,7 +23,7 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Authorization'],
 
     'max_age' => 0,
 
