7f2435040fbb8b76734ac1e724fccd716c1da3d3
joaoLucasPereiraCordeiro <joaolucaspereiracordeiro@gmail.com>
2025-11-08 15:19:27 -0300

    Ajuste CORS

--- todolist-backend/config/cors.php
+++ todolist-backend/config/cors.php
@@ -6,13 +6,14 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://my-taskly-teal.vercel.app',
         'https://my-taskly-g79ptsw12-joao-lucas-projects-1bb67238.vercel.app',
-        'http://localhost:5173', // se for testar local
+        'http://localhost:5173',
+        'http://localhost:8080',
     ],
 
     'allowed_headers' => ['*'],
-
     'exposed_headers' => ['Authorization'],
-
+    'max_age' => 0,
     'supports_credentials' => true,
 ];
