afd0c96248a987d4b808a7025ae0ca2ff5fbfbc1
joaoLucasPereiraCordeiro <joaolucaspereiracordeiro@gmail.com>
2025-12-22 20:19:11 -0300

    erro na model

--- todolist-backend/app/Models/User.php
+++ todolist-backend/app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
         'email',
         'password',
         'role',
-        'last_login',
+        'last_login_at',
     ];
 
     protected $hidden = [
@@ -25,9 +25,8 @@ class User extends Authenticatable
     ];
 
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    'email_verified_at' => 'datetime',
+];
 
     public function tasks()
     {
